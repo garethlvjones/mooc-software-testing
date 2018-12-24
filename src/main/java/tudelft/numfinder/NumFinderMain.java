b9f79ed26360849caf66e2f402f9cd02236b5ee4
garethlvjones <garethlvjones@gmail.com>
2018-12-24 13:31:24 +1100

    Refined numfinder, added test

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -10,8 +10,20 @@ public class NumFinderMain {
 
         // this crashes
         nf.find(new int[] {4, 3, 2, 1});
+        System.out.print("4,3,2,1 output:");
+        System.out.print("Largest: ");
+        System.out.print(nf.getLargest());
+        System.out.print(" smallest: ");
+        System.out.print(nf.getSmallest());
 
-        System.out.println(nf.getLargest());
-        System.out.println(nf.getSmallest());
+        System.out.println();
+        NumFinder nf2 = new NumFinder();
+
+        nf2.find(new int[] {0});
+        System.out.print("null output:");
+        System.out.print("Largest: ");
+        System.out.print(nf2.getLargest());
+        System.out.print(" smallest: ");
+        System.out.print(nf2.getSmallest());
     }
 }
