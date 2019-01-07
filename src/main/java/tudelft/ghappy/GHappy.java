d884c160fbc5ac262287dbc003f48f9712eaceda
139420 <139420@172.19.166.36>
2019-01-07 15:27:30 +1100

    Added tests for Ghappy Fixed ghappy main program

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -4,15 +4,22 @@ public class GHappy {
 
     public boolean gHappy(String str) {
         assert str!=null;
+
+        if (!str.contains("g")) {
+            return true;
+        }
+
         for(int i = 0; i < str.length(); i++) {
             if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
-                if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
+                if (i >= 1 && str.charAt(i-1) == 'g') {
+                    continue;
+                }
+                if (i+1 < str.length() && str.charAt(i+1) == 'g') {
+                    continue;
+                }
                 return false;
             }
         }
-
         return true;
-
     }
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,36 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+
 public class GHappyTest {
+    private GHappy gh;
+
+    @BeforeEach
+    public void before() {
+        gh = new GHappy();
+    }
+
+    @ParameterizedTest(name = "{0}: ({1} = {2}")
+    @CsvSource({
+            "'no Gs at all','xxxx',True",
+            "'One g','xxxgxx', False",
+            "'two Gs and one G', 'xxggxxgx', False",
+            "'one g as last char', 'xxxg', False",
+            "'gs at start', 'ggxx', True",
+            "'gs at end', 'xxxgg', True",
+            "'one set of two gs', 'xggxxx', True",
+            "'two sets of two gs', 'xggxggxx', True",
+            "'three gs', 'xgggxx', True"
+    })
+    public void gTestAlgorithm(String testName, String testPhrase, boolean expectedResult) {
+        boolean result = gh.gHappy(testPhrase);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
+
